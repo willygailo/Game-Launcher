31c92f120b153d78bc46fcc92cda846e594d99d9
willygailo01 <willygailo01@gmail.com>
2026-08-04 10:29:47 +0800

    feat: update app assets, add UI colors, and integrate IpadViewScalerEngine into game monitor service

--- android/app/src/main/java/com/gamebooster/app/gamespace/AutoGameMonitorService.java
+++ android/app/src/main/java/com/gamebooster/app/gamespace/AutoGameMonitorService.java
@@ -109,6 +109,7 @@ public class AutoGameMonitorService extends Service {
                         + profile.label + " up to " + targetHz + "Hz");
 
                 com.gamebooster.app.spoofer.DeviceSpooferEngine.applySpoofing(getApplicationContext(), currentPackage);
+                com.gamebooster.app.engine.IpadViewScalerEngine.applyForGame(getApplicationContext(), currentPackage);
                 com.gamebooster.app.engine.RefreshRateOverrideEngine.applyRefreshRate(getApplicationContext(), currentPackage,
                         targetHz >= 165 ? com.gamebooster.app.engine.RefreshRateOverrideEngine.RefreshRateMode.MODE_165HZ :
                         targetHz >= 144 ? com.gamebooster.app.engine.RefreshRateOverrideEngine.RefreshRateMode.MODE_144HZ :
